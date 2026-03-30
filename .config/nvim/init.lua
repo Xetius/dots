ad8c1b94bd2cd466795dabf1a4fc525d41c52414
Chris Hudson <c.hudson@teranode.group>
2026-03-30 08:24:27 +0100

    More updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,23 +1,5 @@
-vim.filetype.add({
-  extension = {
-    gowork = "gowork",
-    gotmpl = "gotmpl",
-    tfvars = "terraform-vars",
-  },
-  pattern = {
-    [".*%.go%.tmpl"] = "gotmpl",
-    ["docker%-compose%.yml"] = "yaml.docker-compose",
-    ["docker%-compose%.yaml"] = "yaml.docker-compose",
-    ["compose%.yml"] = "yaml.docker-compose",
-    ["compose%.yaml"] = "yaml.docker-compose",
-    ["%.gitlab%-ci%.yml"] = "yaml.gitlab",
-    ["%.gitlab%-ci%.yaml"] = "yaml.gitlab",
-    ["values%.yml"] = "yaml.helm-values",
-    ["values%.yaml"] = "yaml.helm-values",
-  },
-})
-
-require("config.global")
-require("config.options")
-require("config.keymap")
+require('config.config')
+require('config.global')
+require('config.options')
+require('config.keymap')
 
